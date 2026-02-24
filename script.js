28bb98781ca3f196d3c9123df94915bd9b36c72a
Hezron Nyakeruma <hezronnyakeruma@gmail.com>
2026-02-24 09:59:08 +0300

    interactive portfolio

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Professional portfolio loaded.");
+function scrollToContact() {
+document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+}
+
+window.addEventListener("scroll", function(){
+const bars = document.querySelectorAll(".bar");
+bars.forEach(bar => {
+const rect = bar.getBoundingClientRect();
+if(rect.top < window.innerHeight){
+bar.style.width = bar.getAttribute("data-width");
+}
+});
+});
